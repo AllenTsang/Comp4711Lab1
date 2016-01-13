1e1cc4e244851a97a3639787ec3bb78a1076e447
Allen Tsang <allen_tsang@hotmail.com>
2016-01-12 16:24:39 -0800

    Index file fix

--- index.php
+++ index.php
@@ -9,9 +9,3 @@
         ?>
     </body>
 </html>
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
